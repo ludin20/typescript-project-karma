15e4a4cbc6b97d8a74085805e6ecc66a748bc429
ludin20 <oswellgrundmeier42@gmail.com>
2020-08-29 03:03:43 -0700

    change filter

--- src/ui/common/NFTFilter.tsx
+++ src/ui/common/NFTFilter.tsx
@@ -10,7 +10,7 @@ const Container = styled(Row).attrs({
   ${props =>
     props.bordered &&
     css`
-      width: 1030px;
+      width: 930px;
       padding-bottom: 10px;
       padding-top: 30px;
       position: relative;
@@ -38,10 +38,23 @@ const collectionbutton = css`
   }
 `;
 
-const Toggle = styled.button<{ toogled: boolean }>`
+const wrap = css`
+  display: inline;
+`;
+
+const RightToggle = styled.button<{ toogled: boolean }>`
   background: none;
-  width: 20px;
+  width: 50px;
   transform: rotate(270deg);
+  img {
+    width: 14px;
+  }
+`;
+
+const LeftToggle = styled.button<{ toogled: boolean }>`
+  background: none;
+  width: 50px;
+  transform: rotate(90deg);
   img {
     width: 14px;
   }
@@ -59,15 +72,21 @@ interface Props {
 const NFTFilter: React.FC<Props> = ({ children, bordered = true, size = 'default', shouldHideHeader, color, changeNft, setLoading }) => {
   const [collections, setCollections] = React.useState([]);
   const [showData, setShowData] = React.useState([]);
+  const textInput = useRef();
+  const allbutton = useRef();
   const [idx, setIdx] = React.useState(1);
   useEffect(() => {
     axios.get(`https://wax.api.atomicassets.io/atomicmarket/v1/stats/collections?collection_whitelist=alexartworks,alien.worlds,anyo.b1,aprenticeart,athalloffame,atomic,badcryptonft,badges.b1,boxmazeonwax,cheesmansart,chibisglobal,classeartacr,deadbabiesbg,dogpatchbrew,dopestickers,dripnwithwax,eosnationama,eosnhotsauce,giselxtotems,goblinsonwax,gpk.topps,inspectorgen,kaleidoscope,kogsofficial,krwingerarts,matthewsmaze,momentintime,niftywizards,officialhero,organicpics1,parodykidss2,pepe.hero,pokeyforever,radiosilence,ruummspringa,shipplememes,solarsystem1,splintercard,stickypeople,tenminuteart,theonlykarma,tone2tonenft,uplandfanart,uplandhotels,upliftart,uptonemotors,vigilantnfts,waxartcards1&sort=volume&order=desc&symbol=WAX`)
     .then(res => {
       const result = res.data.data.results;
       // result.unshift({'collection_name': 'All Collectinos'});
-      setCollections(result);
-      const temp = res.data.data.results.filter((item, index) => index < 8);
-      setShowData(temp);
+      // setCollections(result);
+      // const temp = res.data.data.results.filter((item, index) => index < 8);
+      const collectionArea = textInput.current.childNodes[0];
+      const firstbutton = allbutton.current;
+      firstbutton.click();
+      firstbutton.focus();    
+      setShowData(result);
     });
   }, []);
   
@@ -79,7 +98,7 @@ const NFTFilter: React.FC<Props> = ({ children, bordered = true, size = 'default
   }, []);
 
   const handleClick = (collection_name) => {
-    if (collection_name == "All Collectinos") {
+    if (collection_name == "all") {
       axios.get('https://wax.api.atomicassets.io/atomicmarket/v1/sales?state=1&max_assets=1&limit=40&page=1&order=desc&sort=created&collection_name=&symbol=WAX&collection_blacklist=caricaturexx,election2o2o,garbagepailz,jesusnft1234,jipewrgvpiwp,kogsfail1234,metalslugnft,monalisanft1,pokemon12345,realelection,safexnft5555,streetfighte,varievarie12,waxforerotic,wwaxxxbabess&show_seller_contracts=false&contract_whitelist=niftywizards,sales.heroes,theonlykarma&seller_blacklist=')
       .then(res => {
         changeNft(res.data.data);
@@ -92,34 +111,40 @@ const NFTFilter: React.FC<Props> = ({ children, bordered = true, size = 'default
     }
   }; 
 
-  const toggleClick = () => {  
-    setIdx(idx+1);    
-    const temp = collections.filter((item, index) => (index >= ((idx+1) * 8) &&  index < ((idx+2) * 8)));
-    if (temp.length < 7) {
-      const temp = collections.filter((item, index) => index < 8);
-      setShowData(temp);
-      setIdx(0);
-    } else {
-      setShowData(temp);
+  const toggleRightClick = () => {  
+    if (idx < 19) {
+      setIdx(idx+1);    
+      const collectionArea = textInput.current.childNodes[0];
+      collectionArea.scrollTo({left: idx*300});
     }
   }
 
-  const handleScroll = () => {
+  const toggleLeftClick = () => { 
+    if (idx > 1) {
+      setIdx(idx-1);
+      const collectionArea = textInput.current.childNodes[0];
+      collectionArea.scrollTo({left: idx*300});
+    }
   }
-
   return (
     <>
-      <Container bordered={bordered} size={size} onScroll={() => handleScroll()}>
-        {
-          showData.map((item, index) => (
-            <button key={index} css={collectionbutton} button onClick={() => handleClick(item.collection_name)}>{item.collection_name}</button>
-          ))
-        }
-      </Container>
-        
-      <Toggle onClick={() => toggleClick()}>
+      <LeftToggle onClick={() => toggleLeftClick()}>
         <img src={arrow} alt="toogle" />
-      </Toggle>
+      </LeftToggle>
+      <div css={wrap} ref={textInput}>
+        <Container bordered={bordered} size={size}>
+          <button css={collectionbutton} button onClick={() => handleClick("all")} ref={allbutton}>All</button>
+          {
+            showData.map((item, index) => (
+              <button key={index} css={collectionbutton} button onClick={() => handleClick(item.collection_name)}>{item.collection_name}</button>
+            ))
+          }
+        </Container>
+      </div>
+        
+      <RightToggle onClick={() => toggleRightClick()}>
+        <img src={arrow} alt="toogle" />
+      </RightToggle>
     </>
   );
 };
